3bc355150dd461cbffff6935771935994e736fdb
anas derk <anas.derk2020@gmail.com>
2023-01-05 10:10:50 -0800

    Config

--- models/DB_URL.js
+++ models/DB_URL.js
@@ -1,3 +1,3 @@
-const DB_URL = "mongodb://localhost:27017/out-circle";
+const DB_URL = "mongodb://ulimac2mpvuprour2xxw:lHKo6VIAyOaOs21DHSnu@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bsuul0a14dhboe8?replicaSet=rs0";
 
 module.exports = DB_URL;
